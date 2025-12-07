3c21cafcaba73526f9917332faf67ffc6f0852ea
alexsusanu78 <alexsusanu78@gmail.com>
2025-12-07 12:32:07 +0200

    updated db url and removed target folder

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-MAVEN_LOCAL_REPO = '/tmp/maven-repo'
-
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -Dmaven.repo.local=${MAVEN_LOCAL_REPO} -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -Dmaven.repo.local=${MAVEN_LOCAL_REPO} -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
