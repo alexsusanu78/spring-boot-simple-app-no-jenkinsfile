5abecae93bc470ffc07545933bc34c4c849df266
alexsusanu78 <alexsusanu78@gmail.com>
2025-12-07 13:18:07 +0200

    updated pom.xml

--- src/main/java/com/wasim/rest/api/entity/Student.java
+++ src/main/java/com/wasim/rest/api/entity/Student.java
@@ -15,12 +15,7 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "email")
     private String email;
-
-    @Column(name = "address")
     private String address;
 }
